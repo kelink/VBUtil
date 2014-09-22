61cfe063af575488809821b4aab197590cf2c0c8
kelink <kelink@users.noreply.github.com>
2014-09-22 18:12:20 +0800

    Update WinScp.sh

--- WinScp.sh
+++ WinScp.sh
@@ -1,5 +1,5 @@
 option confirm off 
-open zawdtena:Zxcv$321@130.47.9.155
+open user:password@IPAddress
 call ps aux | less > /tmp/EnvironmentProcess2014.txt
 call fdisk & disk - l  & df > /tmp/EnvironmentDisk2014.txt
 get /tmp/EnvironmentProcess2014.txt WinSCP\EnvironmentProcess2014.txt
